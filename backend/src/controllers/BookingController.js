51cf72ecdbda3f3bc1ea95107700fa791ebc3d6a
Wellington Garcia <garciawell@gmail.com>
2019-10-06 22:23:59 -0300

    Removendo log

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -5,9 +5,6 @@ module.exports = {
     const { user_id } = req.headers;
     const { spot_id } = req.params;
     const { date } = req.body;
-    console.log("REQ", req.body);
-    console.log("REQ2", user_id);
-    console.log("REQ3", spot_id);
 
     const booking = await Booking.create({
       user: user_id,
